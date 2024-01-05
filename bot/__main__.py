959f5654fae01ee0b28c34301f67f33ad6b07950
icetee4eva <30740369+prablordeppey@users.noreply.github.com>
2024-01-05 22:45:09 +0000

    [upd] attempt to fix server auto-reload failed

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,6 +5,6 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    config = uvicorn.Config("bot.main:app", port=5000, log_level="info")
+    config = uvicorn.Config("bot.main:app", port=5000, log_level="info", reload=True)
     server = uvicorn.Server(config)
     server.run()
