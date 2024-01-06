8dc1f49e0728ae0a2da5cce9b9d8302c6aaa022b
icetee4eva <30740369+prablordeppey@users.noreply.github.com>
2024-01-06 10:11:00 +0000

    [new] tests for get_exchange_urls and get_tradablepairs

---
+++ tests/unittests/schemas/test_get_exchangeurls_schema.py
@@ -0,0 +1,82 @@
+from unittest.mock import patch
+import pytest
+
+from bot.schemas.get_exchangeurls_schema import ExchangeUrlsModel, GetEndpoints, UrlsListSchema
+
+
+class TestGetExchangeUrlsSchema:
+    def test_essential_url_fields_exist(self):
+        # Prepare
+        expected_urls = ["base_url", "ohlc_data", "tradable_pairs"]
+
+        # Act
+        urls = UrlsListSchema.__annotations__.keys()
+
+        # Assert
+        assert len(urls) == 3
+        assert set(expected_urls) == set(urls)
+
+    def test_exchange_urls_fields_exist(self):
+        """This test implements:
+        - expected exchanges exist
+        - exchanges do not have empty urls
+        """
+        # Prepare
+        expected_exchanges = ["binance", "kraken", "coinbase"]
+
+        # Act
+        exchanges = ExchangeUrlsModel()
+        exchanges_dict = exchanges.__annotations__
+
+        # Assert
+        assert set(expected_exchanges) == set(exchanges_dict.keys())
+        for exchange in expected_exchanges:
+            exchange_data = getattr(exchanges, exchange)
+            exchange_data_dict = getattr(exchanges, exchange).__annotations__
+            for url in exchange_data_dict.keys():
+                assert getattr(exchange_data, url) != ""
+
+    @pytest.mark.parametrize(
+        "exchange, expected_response",
+        [
+            ("binance", "binance"),
+            ("kraken", "kraken"),
+            ("coinbase", "coinbase"),
+        ],
+    )
+    def test_get_endpoint_validate_exchange_ok(self, exchange, expected_response):
+        """TODO: Improve this with mocking. business case instead of checking the returns"""
+        # Prepare
+        # Act
+        result = GetEndpoints(exchange=exchange)
+
+        # Assert
+        assert result.exchange == expected_response
+
+    def test_get_endpoint_validate_exchange_fail(self):
+        """TODO: Improve this with mocking"""
+        # Act
+        with pytest.raises(ValueError):
+            GetEndpoints(exchange="invalid_exchange")
+
+    def test_build_tradable_assets_url(self):
+        """TODO: build around business logic"""
+        # Act
+        res_obj = GetEndpoints(exchange="binance")
+        result = res_obj.build_tradable_assets_url()
+
+        # Assert
+        assert isinstance(result, str) is True
+
+    def test_build_ohlc_assets_url(self):
+        """TODO: Poorly written, build around business logic"""
+        # Act
+        res_binance = GetEndpoints(exchange="binance")
+        result_binance = res_binance.build_ohlc_assets_url(pair="BTC-USDT")
+
+        res_coinbase = GetEndpoints(exchange="coinbase")
+        result_coinbase = res_coinbase.build_ohlc_assets_url(pair="BTC-USDT")
+
+        # Assert
+        assert isinstance(result_binance, str) is True
+        assert isinstance(result_coinbase, str) is True
---
+++ tests/unittests/schemas/test_get_tradablepairs_schema.py
@@ -0,0 +1,94 @@
+from unittest.mock import Mock, patch
+from pydantic import ValidationError
+import pytest
+
+from bot.schemas.get_exchangeurls_schema import ExchangeUrlsModel, GetEndpoints, UrlsListSchema
+from bot.schemas.get_tradablepairs_schema import GetTradableAssetPairsSchema
+
+
+@pytest.fixture
+def valid_exchange_urls():
+    return ExchangeUrlsModel(
+        exchange1=UrlsListSchema(
+            base_url="valid_base_url", tradable_pairs="valid_tradable_pairs_url", ohlc_data="valid_ohlc_data_url"
+        ),
+        exchange2=UrlsListSchema(
+            base_url="valid_base_url", tradable_pairs="valid_tradable_pairs_url", ohlc_data="valid_ohlc_data_url"
+        ),
+    )
+
+# @pytest.fixture
+# def valid_binance_asset_pairs_resp_data():
+
+
+
+class TestGetTradablePairsSchema:
+    def test_instance_creation_ok(self):
+        """
+        Test that an instance of GetTradableAssetPairsSchema is created successfully.
+        """
+        # Act
+        obj = GetTradableAssetPairsSchema(exchange="binance")
+
+        # Assert
+        assert isinstance(obj, GetTradableAssetPairsSchema)
+
+    def test_instance_creation_invalid_exchange(self):
+        """
+        Test that ValidationError is raised when an invalid exchange is provided.
+        """
+        # Act and Assert
+        with pytest.raises(ValidationError) as context:
+            GetTradableAssetPairsSchema(exchange="invalid_exchange")
+
+        assert isinstance(context.value, ValidationError)
+
+    @pytest.mark.parametrize(
+        "exchange, json_response",
+        [
+            ("binance", {"symbols": [{"status": "TRADING", "baseAsset": "BTC", "quoteAsset": "USDT"}]}),
+            ("kraken", {"result": {"pair1": {"status": "online", "wsname": "BTC/USDT"}}}),
+            ("coinbase", [{"status": "online", "base_currency": "BTC", "quote_currency": "USDT"}]),
+        ] 
+    )
+    @patch('bot.schemas.get_exchangeurls_schema.GetEndpoints.build_tradable_assets_url')
+    @patch('bot.schemas.get_tradablepairs_schema.requests.get')
+    def test_binance_tradable_pairs(self, mock_requests_get, mock_tradable_assets_url, exchange, json_response):
+        """Test correct parsing of json response
+
+        Args:
+            mock_requests_get (_type_): mocked function
+            mock_tradable_assets_url (_type_): mocked function
+            exchange (str): exchange platform
+            json_response (dict): response from exchange server
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = json_response
+        mock_requests_get.return_value = mock_response
+        mock_tradable_assets_url.return_value = "dummy_url"
+
+        # Instantiate your class
+        obj = GetTradableAssetPairsSchema(exchange=exchange)
+        tradable_pairs = obj.get_tradable_pairs_for_exchange()
+
+        # Assertions
+        assert tradable_pairs == ["BTC/USDT"]
+        mock_tradable_assets_url.assert_called_once_with()
+
+    @patch('bot.schemas.get_exchangeurls_schema.GetEndpoints.build_tradable_assets_url')
+    @patch('bot.schemas.get_tradablepairs_schema.requests.get')
+    def test_failed_processing_tradable_pairs_exception_raised(self, mock_requests_get, mock_tradable_assets_url):
+        """
+        Test that an exception is raised when there is an error fetching tradable pairs.
+        """
+        # Instantiate your class
+        obj = GetTradableAssetPairsSchema(exchange="binance")
+        mock_requests_get.side_effect = Exception("An error occurred while fetching tradable pairs")
+        mock_tradable_assets_url.return_value = "dummy_url"
+
+        # Mock build_tradable_assets_url to return a dummy URL
+        with pytest.raises(Exception) as context:
+            obj.get_tradable_pairs_for_exchange()
+
+        assert isinstance(context.value, ValueError) is True
+        mock_tradable_assets_url.assert_called_once_with()
